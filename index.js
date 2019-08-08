978462f7be99b11a636981ef5c6e23e93abd5fd7
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-08 16:07:15 -0700

    implemented env

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// require("dotenv").config();
-// const server = require("./api/server");
-
 const server = require("./server");
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`API running on port ${port}.`));
