a0f053fedba947cf2d653ec59a81dd118e07e97b
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-08 16:41:06 -0700

    bug fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
+// const express = require("express");
 const server = require("./server");
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 server.listen(port, () => console.log(`API running on port ${port}.`));
