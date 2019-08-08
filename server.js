ae7d1d5bdafd62df551a4543ef108a9399941162
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-08 16:17:06 -0700

    adds key

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.status(200).json({ api: "running" });
+  res.status(200).json({ secretMessage: process.env.MESSAGE });
 });
 
 module.exports = server;
