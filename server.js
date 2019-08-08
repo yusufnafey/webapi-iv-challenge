33f8adb5d9990214d2e0a4fb3821d7599e953467
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-08 16:32:13 -0700

    changed message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.status(200).json({ secretMessage: process.env.MESSAGE });
+  res.status(200).json({ message: "hello" });
 });
 
 module.exports = server;
